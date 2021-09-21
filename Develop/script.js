902ece057550f936c65718b8fb0a1e0c3a22d2da
Leithen <leithen113@gmail.com>
2021-09-20 18:34:53 -0600

    Returned generatePassword result. Starting on defining character sets

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,9 @@
 // Assignment code here
 var generatePassword = function() {
+  debugger;
   var passLength = window.prompt("Please choose the length of password you would like to generate.");
-  passLength = parseInt(passLength);
+  passLength = parseInt(passLength, 10);
+  
   if (isNaN(passLength)){
     window.alert("Sorry, that is not a valid number! Please enter a numeric value.");
     generatePassword();
@@ -10,6 +12,18 @@ var generatePassword = function() {
     window.alert("You typed an INT, congrats!");
     window.alert(passLength);
   }
+
+  var charLower = window.confirm("Would you like to include LOWERCASE characters?");
+ 
+  var charUpper = window.confirm("Would you like to include UPPERCASE characters?");
+  
+  var charNumeric = window.confirm("Would you like to include NUMERIC characters?");
+  
+  var charSpecial = window.confirm("Would you like to include SPECIAL characters?");
+
+
+  //Variable returned will become the password
+  return passLength;
 }
 
 // Get references to the #generate element
